ee5f84771c1e84d8847ec4ccad6f60a895af9c0e
Abdul Azeem <abdulazeem3802@gmail.com>
2025-10-02 11:20:06 +0530

    improve code

--- src/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-// src/routes/authRoutes.js
-import express from 'express';
-import { EmployerLogin } from '../controllers/authController.js';
-
-const router = express.Router();
-
-router.post('/employer/login', EmployerLogin);
-
-export default router;
