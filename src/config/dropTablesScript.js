a26c3fe641ce799c76173f89725f1717fc6db280
AbdulDevnexus9634 <abdulazeem@devnexus.in>
2025-10-25 20:15:51 +0530

    Update dropTablesScript.js

--- src/config/dropTablesScript.js
+++ src/config/dropTablesScript.js
@@ -1,28 +1,21 @@
 import './config.js';
 
 import {
-<<<<<<< HEAD
   dropAllTables,
-  // dropSingleTable,
-=======
   // dropAllTables,
   dropSingleTable,
->>>>>>> 25f851ac7d721537ea311ef8d52d1e578de77e08
 } from './dropTables.js';
 
 const run = async () => {
   try {
-<<<<<<< HEAD
     await dropAllTables();
 
     // OR drop a single table:
     // await dropSingleTable('users');
-=======
     // await dropAllTables();
 
     // OR drop a single table:
     await dropSingleTable('jobs');
->>>>>>> 25f851ac7d721537ea311ef8d52d1e578de77e08
     process.exit(0);
   } catch (err) {
     console.error(err);
